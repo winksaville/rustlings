5a530c56ca58144d42d3eded10c5efab6ea17ab5
Wink Saville <wink@saville.com>
2020-12-08 14:27:07 -0800

    Fix move semantics

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,12 +1,24 @@
 // move_semantics1.rs
 // Make me compile! Execute `rustlings hint move_semantics1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    //let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    // Other ways to initialize to empty with default capacity
+    //let vec0 = vec![];
+    //let vec0 = vec!();
+    //let vec0: Vec<i32> = vec![];
+    //let vec0: Vec<i32> = vec!();
+    //let vec0 = Vec::<i32>::new();
+
+    // You can also set the capacity explicitly, which might
+    // enhance performance or save space.
+    //let vec0 = Vec::with_capacity(0);
+    let vec0 = Vec::with_capacity(4);
+
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -15,12 +27,23 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
+//fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//    let mut vec = vec;
+//
+//    vec.push(22);
+//    vec.push(44);
+//    vec.push(66);
+//
+//    vec
+//}
+
+// Make the "move" inside fill_vec obvious
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vecx = vec;
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    vecx.push(22);
+    vecx.push(44);
+    vecx.push(66);
 
-    vec
+    vecx
 }
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,19 +2,45 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    // hint 1: clone vec0 (I figured this out :)
+    // but thought there were other ways so looked at the hint
+    {
+        let vec0 = Vec::new();
+        let mut vec1 = fill_vec(vec0.clone());
+        println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+        vec1.push(88);
+        println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    }
 
-    let mut vec1 = fill_vec(vec0);
+    // hint2
+    {
+        let vec0 = Vec::new();
+        let mut vec1 = dup_fill_vec(&vec0);
+        println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+        vec1.push(88);
+        println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    }
 
-    // Do not change the following line!
-    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    // hint 3a: pass mutable vec0 and don't return anything
+    {
+        let mut vec0 = Vec::new();
+        append_vec(&mut vec0);
+        println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+        vec0.push(88);
+        println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    }
 
-    vec1.push(88);
-
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    // hint 3b
+    {
+        let vec0 = &mut Vec::new();
+        append_vec(vec0);
+        println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+        vec0.push(88);
+        println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    }
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
@@ -25,4 +51,20 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+fn dup_fill_vec(cur_vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = cur_vec.clone();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+
+fn append_vec(vec: &mut Vec<i32>) {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 }
