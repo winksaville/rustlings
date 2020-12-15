b58936a5f582a538bc5ddc124f7d8f109e13a71c
Wink Saville <wink@saville.com>
2020-12-15 13:54:27 -0800

    Answers for standard_library_types
    iterators4.rs was particularly instructive.
    
    I started out by searching for the initial C recursion and for loop
    versions and tried to use a range iteration but couldn't figure the
    syntax. So I created the first 7 answers and then used hint.
    
    I then seached for: how to use iterator for side effects only
     https://www.google.com/search?q=how+to+use+iterator+for+side+effects+only+in+rust
    
    And saw this:
     https://github.com/rust-lang/rust/issues/44546#issuecomment-344192206
    And:
     https://github.com/rust-lang/rust/issues/44546#issuecomment-381678863
    
    And with that I implemented Answer 8
    
    But still wasn't happy as I felt there must be a better way where I didn't
    need to throw away things. So I looking at the iterator documentation:
     https://doc.rust-lang.org/std/iter/trait.Iterator.html
    
    I saw "fold" and low and behold that was exactly what I needed! Then I
    saw "fold_first" and it was "simpler" as the first value in the range
    is the initial value for the accumulator so fold_first only has one
    parameters not two. But fold_first returns an Option which means I need
    to return the unwrap(). Anyway both Answers 9 and 10 are simpler then
    any other solution!
    
    I do wonder which is fastest.

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -4,18 +4,20 @@
 // somewhere. Try not to create any copies of the `numbers` Vec!
 // Execute `rustlings hint arc1` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
-    let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let numbers: Vec<_> = (0..100u64).collect();
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
+    // Answer 1
     for offset in 0..8 {
+        let child_numbers = shared_numbers.clone();
         joinhandles.push(thread::spawn(move || {
             let mut i = offset;
             let mut sum = 0;
@@ -26,6 +28,25 @@ fn main() {
             println!("Sum of offset {} is {}", offset, sum);
         }));
     }
+
+    //// Answer 2
+    //let threads = 10;
+    //let shard_size = shared_numbers.len() / threads;
+    //if shared_numbers.len() % threads != 0 {
+    //    panic!("shard_size isn't a multiple of numbers.len");
+    //}
+    //for offset in 0..threads {
+    //    let child_numbers = shared_numbers.clone();
+    //    joinhandles.push(thread::spawn(move || {
+    //        let base = shard_size * offset;
+    //        let mut sum = 0;
+    //        for i in 0..shard_size {
+    //            sum += child_numbers[base + i];
+    //        }
+    //        println!("Sum of offset {} is {}", offset, sum);
+    //    }));
+    //}
+
     for handle in joinhandles.into_iter() {
         handle.join().unwrap();
     }
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,6 +1,147 @@
+#![feature(iterator_fold_self)]
 // iterators4.rs
 
-// I AM NOT DONE
+// I AM DONE
+
+pub fn recursive(num: u64) -> u64 {
+    let mut fact = num;
+    if num > 1 {
+        fact = num * recursive(num - 1)
+    }
+    println!("recursive: num={} fact={}", num, fact);
+    fact
+}
+
+pub fn simplest_recursive(num: u64) -> u64 {
+    if num > 1 {
+        num * simplest_recursive(num - 1)
+    } else {
+        num
+    }
+}
+
+pub fn match_recursive(num: u64) -> u64 {
+    match num {
+        n if n > 1 => n * match_recursive(n - 1),
+        n => n,
+    }
+}
+
+pub fn for_in(num: u64) -> u64 {
+    let mut fact = 1;
+    for i in 1..num+1 {
+        fact *= i;
+        println!("for_in: num={} fact={}", num, fact);
+    }
+    fact
+}
+
+// Create iterable version of Factorial
+
+struct Factorial {
+    num: u64,
+    factorial: u64,
+}
+
+impl Factorial {
+    fn new(num: u64) -> Factorial {
+        Factorial {
+            num,
+            factorial: 1
+        }
+    }
+}
+
+impl Iterator for Factorial {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        println!("Factorial::next:+ self.num={}", self.num);
+        if self.num > 0 {
+            self.factorial *= self.num;
+            self.num -= 1;
+            println!("Factorial::next:- Some self.factorial={}", self.factorial);
+            return Some(self.factorial)
+        } else {
+            println!("Factorial::next:- None self.factorial={}", self.factorial);
+            return None
+        }
+    }
+}
+
+pub fn loop_next_iterator(num: u64) -> u64 {
+    let mut f = Factorial::new(num);
+    loop {
+        let n = f.next();
+        if n == None {
+            break;
+        }
+        println!("iterator: f.next()={}", n.unwrap());
+    }
+    println!("iterator:- f.factorial={}", f.factorial);
+    f.factorial
+}
+
+pub fn for_in_iterator(num: u64) -> u64 {
+    println!("iterator:+ num={}", num);
+    let f = Factorial::new(num);
+    let mut x: u64 = 1;
+    for curf in f {
+        println!("iterator: curf={}", curf);
+        x = curf;
+    }
+    println!("iterator:- x={}", x);
+    x
+}
+
+pub fn while_next_iterator(num: u64) -> u64 {
+    let mut f = Factorial::new(num);
+    while f.next() != None {}
+    f.factorial
+}
+
+pub fn range_for_each_iterator(num: u64) -> u64 {
+    // "rf" is a a reference to mutable fact
+    // which is captured by the map closure.
+    //let mut fact: u64 = 1;
+    //let rf = &mut fact;
+    //(1..num+1).map(|n| {
+    //    *rf *= n;
+    //    println!("n={} *rf={}", n, *rf);
+    //} ).for_each(drop);
+    //fact
+
+    // Turns out you don't need "rf", I'm guessing because fact
+    // is on the left side of "=" and therfore is areference.
+    let mut fact: u64 = 1;
+    (1..num+1).map(|n| {
+        fact *= n;
+        //println!("n={} fact={}", n, fact);
+    } ).for_each(drop);
+    fact
+}
+
+pub fn range_fold_iterator(num: u64) -> u64 {
+    (1..num+1).fold(1, |fact, n| fact * n)
+
+    //// For debugging add a println
+    //(1..num+1).fold(1, |fact, n| {
+    //    let f = fact * n;
+    //    println!("n={} f={}", n, f);
+    //    f
+    //} )
+}
+
+pub fn range_fold_first_iterator(num: u64) -> u64 {
+    (1..num+1).fold_first(|fact, n| fact * n).unwrap()
+
+    //// For debugging add a println
+    //(1..num+1).fold_first(|fact, n| {
+    //    let f = fact * n;
+    //    println!("n={} f={}", n, f);
+    //    f
+    //} ).unwrap()
+}
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -12,7 +153,40 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    // Answer 1 recursive
+    //recursive(num)
+
+    // Answer 2 simplest recursive
+    //simplest_recursive(num)
+
+    // Answer 3 match recursive
+    //match_recursive(num)
+
+    // Answer 4 for_in no recursion
+    //for_in(num)
+
+    // Answer 5 iterate using loop
+    //loop_next_iterator(num)
+
+    // Answer 6 iterate using for in
+    //for_in_iterator(num)
+
+    // Answer 7 iterate using while
+    //while_next_iterator(num)
+
+    // Answer 8 Iterate using range and for_each
+    //range_for_each_iterator(num)
+
+    // Answer 9 Iterate using range and fold
+    // THIS IS THE BEST ANSWER, or maybe fold_first below.
+    //range_fold_iterator(num)
+
+    // Answer 10 Iterate using range and fold_first
+    // THIS IS THE BEST ANSWER :)
+    range_fold_first_iterator(num)
 }
+ 
 
 #[cfg(test)]
 mod tests {
