9f58852ab99b6a9e8ea2f76243e2e5035b5d0044
Wink Saville <wink@saville.com>
2020-12-16 20:47:31 -0800

    Answers for traits
    I also created benchmarks at rust-benchmarks_experiments:
      https://github.com/winksaville/rust-benchmark_experiments

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -8,14 +8,66 @@
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
+// I AM DONE
+
+#![feature(test)]
+
+extern crate test;
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
+    fn append_bar1(self) -> Self;
+    fn append_bar2(self) -> Self;
+    fn append_bar3(self) -> Self;
+    fn append_bar4(self) -> Self;
 }
 
 impl AppendBar for String {
-    //Add your code here
+    // Default
+    fn append_bar(self) -> Self {
+        self.append_bar2()
+    }
+
+    // Answer 1
+    // This is 85 LOC and only does one alloc or realloc
+    // but needs extra LOC in the non-error path and thus maybe slower.
+    fn append_bar1(self) -> Self {
+        self + "Bar"
+    }
+
+    // Answer 2
+    // This is 75 LOC and only does one alloc or realloc
+    // and has less LOC in the non-error path.
+    fn append_bar2(self) -> String {
+        let mut s = self;
+        s.push_str("Bar");
+        s
+    }
+
+    // Answer 3
+    // This is 162 LOC and I thought this might be "faster" and "smaller"
+    // as it only needs to check for allocation errors on the
+    // "with_capacity" but it still checks the other paths too although
+    // they'll never be excersized (assuming owership works:).
+    // There is a non-error "fast-path" will only do a memcpy if self already
+    // has the capacity.
+    //
+    fn append_bar3(self) -> String {
+        let mut s = String::with_capacity(self.len() + "Bar".len());
+        s.push_str(self.as_str());
+        s.push_str("Bar");
+        s
+    }
+
+    // Answer 4
+    fn append_bar4(self) -> String {
+        let mut s = String::with_capacity(self.len() + "Bar".len());
+        s = self;
+        s.push_str("Bar");
+        s
+    }
+
 }
 
 fn main() {
