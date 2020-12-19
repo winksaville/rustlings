9096963cfd2c90b8c02e793feb4adc6908666944
Wink Saville <wink@saville.com>
2020-12-19 10:20:35 -0800

    Answers for threads
    Answer 3 is from reading the hint for threads1 and is a nicer and more
    general solution.

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -5,26 +5,75 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+
+//// Answer 1
+//use std::sync::Mutex;
+//
+//struct JobStatus {
+//    jobs_completed: Mutex<u32>,
+//}
+//
+//fn main() {
+//    let status = Arc::new(JobStatus { jobs_completed: std::sync::Mutex::new(0) });
+//    let status_shared = status.clone();
+//    thread::spawn(move || {
+//        for _ in 0..10 {
+//            thread::sleep(Duration::from_millis(250));
+//            *status_shared.jobs_completed.lock().unwrap() += 1
+//        }
+//    });
+//    while *status.jobs_completed.lock().unwrap() < 10 {
+//        println!("waiting... ");
+//        thread::sleep(Duration::from_millis(500));
+//    }
+//}
+
+//// Answer 2 using AtomicUsize
+//use std::sync::atomic::{AtomicUsize, Ordering};
+//
+//struct JobStatus {
+//    jobs_completed: AtomicUsize,
+//}
+//
+//fn main() {
+//    let status = Arc::new(JobStatus { jobs_completed: AtomicUsize::new(0) });
+//    let status_shared = status.clone();
+//    thread::spawn(move || {
+//        for _ in 0..10 {
+//            thread::sleep(Duration::from_millis(250));
+//            status_shared.jobs_completed.fetch_add(1, Ordering::SeqCst);
+//        }
+//    });
+//    while status.jobs_completed.load(Ordering::SeqCst) < 10 {
+//        println!("waiting... ");
+//        thread::sleep(Duration::from_millis(500));
+//    }
+//}
+
+// Answer 3 using Mutex on JobStatus as the hint suggests
+// This is definitely what you want if you consider there
+// could be multiple fields in a struct.
+use std::sync::Mutex;
 
 struct JobStatus {
     jobs_completed: u32,
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let status_shared = status.clone();
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
-            status_shared.jobs_completed += 1;
+            status_shared.lock().unwrap().jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
+    while status.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
